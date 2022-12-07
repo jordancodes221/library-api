17b40b6a14ccde3785cc256c1c30144ea9aa6afb
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2022-12-07 13:35:58 -0500

    Made the comments more clear and concise.

--- main.go
+++ main.go
@@ -148,15 +148,10 @@ func updateBook(c* gin.Context) {
 			book.Checkedout_customerID = request_customerID
 			book.Time_updated = time.Now()
 		}
-
 	} else if (current_state == "on-hold") {
-		if (book_onhold_customerID == request_customerID) {
-			// the request comes from the customer who has it on-hold
-			// this customer can check it out or return it - both cases are handled below
-			// the customer could also do an idempotent operation of putting it on-hold
+		if (book_onhold_customerID == request_customerID) { // The request comes from the customer who has it on-hold.
 			book.State = requested_state
 			book.Time_updated = time.Now()
-			// update the "updated time" field of book
 			if (requested_state == "checked-out") {
 				book.Onhold_customerID = ""
 				book.Checkedout_customerID = request_customerID
@@ -166,63 +161,56 @@ func updateBook(c* gin.Context) {
 				book.Onhold_customerID = ""
 				book.Time_updated = time.Now()
 			}
-		} else { // (book_onhold_customerID != request_customerID) && (current_state == "on-hold")
-			// if a customer has the book on-hold, only that customer can check it out or return it
-			// we handled both of these cases above
+		} else { // The request comes from a customer different from the one who has the book on-hold.
+			// Only the customer who has the book on-hold can change its state, so raise an error
 			c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Request failed. Another customer has the book on-hold."})
 		}
-	} else { // current_state == "checked-out"}
-		if (book_checkedout_customerID == request_customerID) {
-			// the book is currently checked out, and a request comes in from the customer who has it
-			// this customer can either return it, or place it on-hold if nobody else
-			// this case is more complicated...
-				// the state cannot be determined simply because the ID's match, unlike the on-hold case
-				// lets say the requested state is available; if someone has it on-hold then it goes to OH
+	} else { // if (current_state) == "checked-out"
+		if (book_checkedout_customerID == request_customerID) { // The request comes from the customer who has it checked-out
 			if (requested_state == "available") {
-				if (book_onhold_customerID == "") { // nobody has it on-hold
+				// In this case, the customer who has it checked-out wishes to return the book (requesting the state is changed to "available")
+				// If another customer has the book on-hold, the book's state will not be changed to "available" but rather to "on-hold" for that customer.
+				if (book_onhold_customerID == "") { // Nobody has the book on-hold
 					book.State = "available"
 					book.Checkedout_customerID = ""
 					book.Time_updated = time.Now()
-				} else { // somebody does have it on-hold
+				} else { // Another customer has the book on-hold
 					book.State = "on-hold"
 					book.Checkedout_customerID = ""
 					book.Time_updated = time.Now()
 				}
 			} else if (requested_state == "on-hold") {
-				if (book_onhold_customerID == "") { // nobody has it on-hold
+				// In this case, the customer who has it checked-out wishes to place it on-hold.
+				// If another customer has the book on-hold, this state change cannot be done.
+				if (book_onhold_customerID == "") { // Nobody has the book on-hold
 					book.State = "on-hold"
 					book.Onhold_customerID = request_customerID
 					book.Checkedout_customerID = ""
 					book.Time_updated = time.Now()
-				} else { // somebody does have it on-hold
+				} else { // Another customer has the book on-hold
 					c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Request failed. Another customer has the book on-hold."})
 				}
-			} else { // new state is checked-out and customers id
-				book.State = "checked-out" // don't really need this, but didn't want to leave it empty
+			} else { // if (requested_state == "checked-out")
+				book.State = "checked-out" // This is an idempotent operation on the book's state.
 				book.Time_updated = time.Now()
 			}
-		} else { // book is checked-out, but somebody else makes a request
-			// if requested state is on-hold but book is checked out, we cannot change the state to
-			// on-hold, but what we can do is update the on-hold customer ID if it is currently ""
-			// i.e. if nobody has it on-hold, the customer can put it on hold without changing its state
+		} else { // The request comes from a customer different from the one who has it checked-out
 			if (requested_state == "available") {
 				c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Request failed. Another customer has the book checked-out."})
 			} else if (requested_state == "on-hold") {
-				// this is the most complex case
-				if (book_onhold_customerID == "") { // nobody has it on-hold
+				// In this case, the book is checked-out and another customer requests to put it on-hold (meanwhile no other customer has it on-hold)
+				// We can update the Onhold_customerID to accomodate this request, but the book's state will remain unchanged (it stays as "checked-out")
+				if (book_onhold_customerID == "") { // Nobody has the book on-hold
 					book.Onhold_customerID = request_customerID
 					book.Time_updated = time.Now()
-				} else { // somebody does have it on-hold
+				} else { // Another customer has the book on-hold
 					c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Request failed. The book is checked-out, and another customer has it on-hold."})
 				}
-			} else { // requested state is to check it out... but it's already checked out
+			} else { // if (requested_state == "available")
 				c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Request failed. Another customer has the book checked-out. Consider placing it on-hold"})
 			}
 		}
 	}
-
-	// how to handle invalid state --> may not have to if you we give drop-down menu on front end??
-
 	c.IndentedJSON(http.StatusOK, book)
 }
 
@@ -237,6 +225,10 @@ func main() {
 	router.Run("localhost:8080")
 }
 
+
+// Clean up the logic... get rid of else statements and the "idempotent state updates"
+
+// Make a new branch where you add a README, with curl commands
 // PATCH request is as follows
 // had an error because the request had State instead of Requested_State in the json
 // NEED TO UPDATE PATCH REQUEST DATA TO MATCH NEW SCHEMA .... NEVERMIND!!!!!
