d30cb3751f1f4ad016d4a406f8f5b405a56473d4
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2022-12-06 19:24:09 -0500

    Modified updateBook function to fill in the Time_updated field of the book struct when an update is made.

--- main.go
+++ main.go
@@ -143,8 +143,10 @@
 			book.State = new_state
 			if (new_state == "on-hold") {
 				book.Onhold_customerID = request_customerID
+				book.Time_updated = time.Now()
 			} else if (new_state == "checked-out") {
 				book.Checkedout_customerID = request_customerID
+				book.Time_updated = time.Now()
 			}
 	
 		} else if (current_state == "on-hold") {
@@ -153,13 +155,16 @@
 				// this customer can check it out or return it - both cases are handled below
 				// the customer could also do an idempotent operation of putting it on-hold
 				book.State = new_state
+				book.Time_updated = time.Now()
 				// update the "updated time" field of book
 				if (new_state == "checked-out") {
 					book.Onhold_customerID = ""
 					book.Checkedout_customerID = request_customerID
+					book.Time_updated = time.Now()
 				}
 				if (new_state == "available") {
 					book.Onhold_customerID = ""
+					book.Time_updated = time.Now()
 				}
 			} else { // (book_onhold_customerID != request_customerID) && (current_state == "on-hold")
 				// if a customer has the book on-hold, only that customer can check it out or return it
@@ -177,20 +182,24 @@
 					if (book_onhold_customerID == "") { // nobody has it on-hold
 						book.State = "available"
 						book.Checkedout_customerID = ""
+						book.Time_updated = time.Now()
 					} else { // somebody does have it on-hold
 						book.State = "on-hold"
 						book.Checkedout_customerID = ""
+						book.Time_updated = time.Now()
 					}
 				} else if (new_state == "on-hold") {
 					if (book_onhold_customerID == "") { // nobody has it on-hold
 						book.State = "on-hold"
 						book.Onhold_customerID = request_customerID
 						book.Checkedout_customerID = ""
+						book.Time_updated = time.Now()
 					} else { // somebody does have it on-hold
 						c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Request failed. Another customer has the book on-hold."})
 					}
 				} else { // new state is checked-out and customers id
 					book.State = "checked-out" // don't really need this, but didn't want to leave it empty
+					book.Time_updated = time.Now()
 				}
 			} else { // book is checked-out, but somebody else makes a request
 				// if requested state is on-hold but book is checked out, we cannot change the state to
@@ -202,6 +211,7 @@
 					// this is the most complex case
 					if (book_onhold_customerID == "") { // nobody has it on-hold
 						book.Onhold_customerID = request_customerID
+						book.Time_updated = time.Now()
 					} else { // somebody does have it on-hold
 						c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Request failed. The book is checked-out, and another customer has it on-hold."})
 					}
