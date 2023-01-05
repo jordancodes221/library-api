f322f5cfa01f1b92a4d5f8e08736f2f81fa25117
Jordan Knorz <jordanknorz@Jordans-MBP-3.hitronhub.home>
2023-01-05 15:54:11 -0500

    Changed JSON fields to pointers.

--- main.go
+++ main.go
@@ -11,57 +11,60 @@ import (
 )
 
 type Book struct{
-	ISBN 			string 	`json:"isbn"`
-	State 			string 	`json:"state"`
+	ISBN 			*string 	`json:"isbn"`
+	State 			*string 	`json:"state"`
 
-	OnHoldCustomerID 	string 	`json:"onholdcustomerid"`
-	CheckedOutCustomerID 	string 	`json:"checkedoutcustomerid"`
+	OnHoldCustomerID 	*string 	`json:"onholdcustomerid"`
+	CheckedOutCustomerID 	*string 	`json:"checkedoutcustomerid"`
 
-	TimeCreated 		string 	`json:"timecreated"`
-	TimeUpdated  		string 	`json:"timeupdated"`
+	TimeCreated 		*time.Time 	`json:"timecreated"`
+	TimeUpdated  		*time.Time	`json:"timeupdated"`
 }
 
+
+
 // Test data
-var bookInstance0 Book = Book{"0000", "available", 	"", 	"", 	time.Now().String(), time.Time{}.String()} // --> Available
-var bookInstance1 Book = Book{"0001", "available", 	"", 	"", 	time.Now().String(), time.Time{}.String()} // --> Checked-out
-var bookInstance2 Book = Book{"0002", "available", 	"", 	"", 	time.Now().String(), time.Time{}.String()} // --> On-hold
+	// TODO: Find a way to instantiate the books with the new JSON ptr fields
+// var bookInstance0 Book = Book{"0000", "available", 	nil,  nil, 	time.Now().String(), time.Time{}.String()} // --> Available
+// var bookInstance1 Book = Book{"0001", "available", 	nil, 	nil, 	time.Now().String(), time.Time{}.String()} // --> Checked-out
+// var bookInstance2 Book = Book{"0002", "available", 	nil, 	nil, 	time.Now().String(), time.Time{}.String()} // --> On-hold
 
-var bookInstance3 Book = Book{"0003", "checked-out", 	"", 	"01", 	time.Now().String(), time.Time{}.String()} // --> Available
-var bookInstance4 Book = Book{"0004", "checked-out", 	"", 	"01", 	time.Now().String(), time.Time{}.String()} // --> Available (no match)
-var bookInstance5 Book = Book{"0005", "checked-out", 	"", 	"01", 	time.Now().String(), time.Time{}.String()} // --> Checked-out
-var bookInstance6 Book = Book{"0006", "checked-out", 	"", 	"01", 	time.Now().String(), time.Time{}.String()} // --> Checked-out (no match)
-var bookInstance7 Book = Book{"0007", "checked-out", 	"", 	"01", 	time.Now().String(), time.Time{}.String()} // --> On-hold 
-var bookInstance8 Book = Book{"0008", "checked-out", 	"", 	"01", 	time.Now().String(), time.Time{}.String()} // --> On-hold (no match)
+// var bookInstance3 Book = Book{"0003", "checked-out", 	nil, 	"01", 	time.Now().String(), time.Time{}.String()} // --> Available
+// var bookInstance4 Book = Book{"0004", "checked-out", 	nil, 	"01", 	time.Now().String(), time.Time{}.String()} // --> Available (no match)
+// var bookInstance5 Book = Book{"0005", "checked-out", 	nil, 	"01", 	time.Now().String(), time.Time{}.String()} // --> Checked-out
+// var bookInstance6 Book = Book{"0006", "checked-out", 	nil, 	"01", 	time.Now().String(), time.Time{}.String()} // --> Checked-out (no match)
+// var bookInstance7 Book = Book{"0007", "checked-out", 	nil, 	"01", 	time.Now().String(), time.Time{}.String()} // --> On-hold 
+// var bookInstance8 Book = Book{"0008", "checked-out", 	nil, 	"01", 	time.Now().String(), time.Time{}.String()} // --> On-hold (no match)
 
-var bookInstance9 Book =  Book{"0009", "on-hold", 	"01", 	"", 	time.Now().String(), time.Time{}.String()} // --> Available
-var bookInstance10 Book = Book{"0010", "on-hold", 	"01", 	"", 	time.Now().String(), time.Time{}.String()} // --> Available (no match)
-var bookInstance11 Book = Book{"0011", "on-hold", 	"01", 	"", 	time.Now().String(), time.Time{}.String()} // --> Checked-out
-var bookInstance12 Book = Book{"0012", "on-hold", 	"01", 	"", 	time.Now().String(), time.Time{}.String()} // --> Checked-out (no match)
-var bookInstance13 Book = Book{"0013", "on-hold", 	"01", 	"", 	time.Now().String(), time.Time{}.String()} // --> On-hold 
-var bookInstance14 Book = Book{"0014", "on-hold", 	"01", 	"", 	time.Now().String(), time.Time{}.String()} // --> On-hold (no match)
+// var bookInstance9 Book =  Book{"0009", "on-hold", 	"01", 	nil, 	time.Now().String(), time.Time{}.String()} // --> Available
+// var bookInstance10 Book = Book{"0010", "on-hold", 	"01", 	nil, 	time.Now().String(), time.Time{}.String()} // --> Available (no match)
+// var bookInstance11 Book = Book{"0011", "on-hold", 	"01", 	nil, 	time.Now().String(), time.Time{}.String()} // --> Checked-out
+// var bookInstance12 Book = Book{"0012", "on-hold", 	"01", 	nil, 	time.Now().String(), time.Time{}.String()} // --> Checked-out (no match)
+// var bookInstance13 Book = Book{"0013", "on-hold", 	"01", 	nil, 	time.Now().String(), time.Time{}.String()} // --> On-hold 
+// var bookInstance14 Book = Book{"0014", "on-hold", 	"01", 	nil, 	time.Now().String(), time.Time{}.String()} // --> On-hold (no match)
 
-var bookInstance15 Book = Book{"0015", "available", 	"", 	"", 	time.Now().String(), time.Time{}.String()} // --> This is the book to be deleted in testing
+// var bookInstance15 Book = Book{"0015", "available", 	nil, 	nil, 	time.Now().String(), time.Time{}.String()} // --> This is the book to be deleted in testing
 
 var mapOfBooks = map[string]*Book{
-	"0000" : &bookInstance0,
-	"0001" : &bookInstance1,
-	"0002" : &bookInstance2,
+	// "0000" : &bookInstance0,
+	// "0001" : &bookInstance1,
+	// "0002" : &bookInstance2,
 
-	"0003" : &bookInstance3,
-	"0004" : &bookInstance4,
-	"0005" : &bookInstance5,
-	"0006" : &bookInstance6,
-	"0007" : &bookInstance7,
-	"0008" : &bookInstance8,
+	// "0003" : &bookInstance3,
+	// "0004" : &bookInstance4,
+	// "0005" : &bookInstance5,
+	// "0006" : &bookInstance6,
+	// "0007" : &bookInstance7,
+	// "0008" : &bookInstance8,
 
-	"0009" : &bookInstance9,
-	"0010" : &bookInstance10,
-	"0011" : &bookInstance11,
-	"0012" : &bookInstance12,
-	"0013" : &bookInstance13,
-	"0014" : &bookInstance14,
+	// "0009" : &bookInstance9,
+	// "0010" : &bookInstance10,
+	// "0011" : &bookInstance11,
+	// "0012" : &bookInstance12,
+	// "0013" : &bookInstance13,
+	// "0014" : &bookInstance14,
 
-	"0015" : &bookInstance15,
+	// "0015" : &bookInstance15,
 }
 
 // GET (all books)
@@ -101,53 +104,53 @@ func GetIndividualBook(c *gin.Context) {
 }
 
 // Input checking
-func inputCheck(book *Book) error {
-	// Consider adding something to check ISBN number format (see: https://pkg.go.dev/github.com/moraes/isbn)
+// func inputCheck(book *Book) error {
+// 	// Consider adding something to check ISBN number format (see: https://pkg.go.dev/github.com/moraes/isbn)
 
-	// check if state is valid
-	if (book.State != "available") && (book.State != "on-hold") && (book.State != "checked-out") {
-		return errors.New("Invalid state in created book.")
-	}
+// 	// check if state is valid
+// 	if (book.State != "available") && (book.State != "on-hold") && (book.State != "checked-out") {
+// 		return errors.New("Invalid state in created book.")
+// 	}
 
-	// if book is available, it cannot have an on-hold or checked-out customer ID
-	if (book.State == "available") {
-		if (book.OnHoldCustomerID != "") && (book.CheckedOutCustomerID == "") {
-			return errors.New("Cannot have an on-hold customer ID on an available book.")
-		}
+// 	// if book is available, it cannot have an on-hold or checked-out customer ID
+// 	if (book.State == "available") {
+// 		if (book.OnHoldCustomerID != "") && (book.CheckedOutCustomerID == "") {
+// 			return errors.New("Cannot have an on-hold customer ID on an available book.")
+// 		}
 
-		if (book.OnHoldCustomerID == "") && (book.CheckedOutCustomerID != "") {
-			return errors.New("Cannot have checked-out customer ID on an available book.")
-		}
+// 		if (book.OnHoldCustomerID == "") && (book.CheckedOutCustomerID != "") {
+// 			return errors.New("Cannot have checked-out customer ID on an available book.")
+// 		}
 
-		if (book.OnHoldCustomerID != "") && (book.CheckedOutCustomerID != "") {
-			return errors.New("Cannot have an on-hold or checked-out customer ID on an available book.")
-		}
-	}
+// 		if (book.OnHoldCustomerID != "") && (book.CheckedOutCustomerID != "") {
+// 			return errors.New("Cannot have an on-hold or checked-out customer ID on an available book.")
+// 		}
+// 	}
 
-	// If new book is on-hold, ensure there is an on-hold customer ID and no checked-out ID
-	if (book.State == "on-hold") {
-		if (book.OnHoldCustomerID == "") {
-			return errors.New("Missing on-hold customer ID.")
-		}
+// 	// If new book is on-hold, ensure there is an on-hold customer ID and no checked-out ID
+// 	if (book.State == "on-hold") {
+// 		if (book.OnHoldCustomerID == "") {
+// 			return errors.New("Missing on-hold customer ID.")
+// 		}
 
-		if (book.CheckedOutCustomerID != "") {
-			return errors.New("Cannot have a checked-out customer ID on an on-hold book.")
-		}
-	}
+// 		if (book.CheckedOutCustomerID != "") {
+// 			return errors.New("Cannot have a checked-out customer ID on an on-hold book.")
+// 		}
+// 	}
 
-	// If new book is checked-out, ensure there is a checked-out customer ID and no on-hold ID
-	if (book.State == "checked-out") {
-		if (book.CheckedOutCustomerID == "") {
-			return errors.New("Missing checked-out customer ID.")
-		}
+// 	// If new book is checked-out, ensure there is a checked-out customer ID and no on-hold ID
+// 	if (book.State == "checked-out") {
+// 		if (book.CheckedOutCustomerID == "") {
+// 			return errors.New("Missing checked-out customer ID.")
+// 		}
 
-		if (book.OnHoldCustomerID != "") {
-			return errors.New("Cannot have an on-hold customer ID on a checked-out book.")
-		}
-	}
+// 		if (book.OnHoldCustomerID != "") {
+// 			return errors.New("Cannot have an on-hold customer ID on a checked-out book.")
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 // POST
 func CreateBook(c *gin.Context) {
@@ -160,21 +163,21 @@ func CreateBook(c *gin.Context) {
 	}
 
 	// Make sure ISBN is not already in-use
-	if _, ok := mapOfBooks[newBook.ISBN]; ok {
+	if _, ok := mapOfBooks[*newBook.ISBN]; ok {
 		c.IndentedJSON(http.StatusConflict, gin.H{"ERROR": "Book already exists."})
 		return
 	}
 
-	// Input checking
-	if err := inputCheck(&newBook); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
-		return
-	}
+	// // Input checking
+	// if err := inputCheck(&newBook); err != nil {
+	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+	// 	return
+	// }
 
-	newBook.TimeCreated = time.Now().String()
-	newBook.TimeUpdated = time.Now().String()
+	*newBook.TimeCreated = time.Now()
+	*newBook.TimeUpdated = time.Now()
 
-	mapOfBooks[newBook.ISBN] = &newBook
+	mapOfBooks[*newBook.ISBN] = &newBook
 
 	c.IndentedJSON(http.StatusCreated, newBook) // 201 status code if successful
 }
@@ -200,24 +203,24 @@ var NoMatchError error = errors.New("ID's do not match.")
 	// on-hold --> checked-out
 	// checked-out --> checked-out
 func Checkout(currentBook *Book, incomingBook *Book) (*Book, error) {
-	if (currentBook.State == "available") {
-		currentBook.State = "checked-out" // or should we use incomingBook.State? 
+	if (*currentBook.State == "available") {
+		*currentBook.State = "checked-out" // or should we use incomingBook.State? 
 		currentBook.CheckedOutCustomerID = incomingBook.CheckedOutCustomerID
-		currentBook.TimeUpdated = time.Now().String()
+		*currentBook.TimeUpdated = time.Now()
 	}
 
-	if (currentBook.State == "on-hold") {
+	if (*currentBook.State == "on-hold") {
 		if (currentBook.OnHoldCustomerID == incomingBook.CheckedOutCustomerID) { // ensure the customer who currently has it on-hold is the same one trying to check it out
-			currentBook.State = "checked-out"
-			currentBook.OnHoldCustomerID = ""
+			*currentBook.State = "checked-out"
+			currentBook.OnHoldCustomerID = nil
 			currentBook.CheckedOutCustomerID = incomingBook.CheckedOutCustomerID
-			currentBook.TimeUpdated = time.Now().String()
+			*currentBook.TimeUpdated = time.Now()
 		} else {
 			return nil, NoMatchError
 		}
 	}
 
-	if (currentBook.State == "checked-out") {
+	if (*currentBook.State == "checked-out") {
 		if (currentBook.CheckedOutCustomerID == incomingBook.CheckedOutCustomerID) { // ensure the customer who currently has it checked out is the same one trying to check it out redundantly
 			// pass
 		} else {
@@ -238,13 +241,13 @@ func Conflict(currentBook *Book, incomingBook *Book) (*Book, error) {
 	// available --> on-hold
 	// on-hold --> on-hold
 func PlaceHold(currentBook *Book, incomingBook *Book) (*Book, error) {
-	if (currentBook.State == "available") {
-		currentBook.State = "on-hold"
+	if (*currentBook.State == "available") {
+		*currentBook.State = "on-hold"
 		currentBook.OnHoldCustomerID = incomingBook.OnHoldCustomerID
-		currentBook.TimeUpdated = time.Now().String()
+		*currentBook.TimeUpdated = time.Now()
 	}
 
-	if (currentBook.State == "on-hold") {
+	if (*currentBook.State == "on-hold") {
 		if (currentBook.OnHoldCustomerID == incomingBook.OnHoldCustomerID) { // ensure the customer who currently has it on-hold is the same one trying to check it out
 			// pass
 		} else {
@@ -258,11 +261,11 @@ func PlaceHold(currentBook *Book, incomingBook *Book) (*Book, error) {
 // ReleaseHold
 	// on-hold --> available (when ID's match)
 func ReleaseHold(currentBook *Book, incomingBook *Book) (*Book, error) {
-	if (currentBook.State == "on-hold") {
+	if (*currentBook.State == "on-hold") {
 		if (currentBook.OnHoldCustomerID == incomingBook.OnHoldCustomerID) {
-			currentBook.State = "available"
-			currentBook.OnHoldCustomerID = ""
-			currentBook.TimeUpdated = time.Now().String()
+			*currentBook.State = "available"
+			currentBook.OnHoldCustomerID = nil
+			*currentBook.TimeUpdated = time.Now()
 		} else {
 			return nil, NoMatchError
 		}
@@ -274,11 +277,11 @@ func ReleaseHold(currentBook *Book, incomingBook *Book) (*Book, error) {
 // Return
 	// checked-out --> available (when ID's match)
 func Return(currentBook *Book, incomingBook *Book) (*Book, error) {
-	if (currentBook.State == "checked-out") {
+	if (*currentBook.State == "checked-out") {
 		if (currentBook.CheckedOutCustomerID == incomingBook.CheckedOutCustomerID) {
-			currentBook.State = "available"
-			currentBook.CheckedOutCustomerID = "" // need this, or leave it as who most recently had it on hold?
-			currentBook.TimeUpdated = time.Now().String()
+			*currentBook.State = "available"
+			currentBook.CheckedOutCustomerID = nil // need this, or leave it as who most recently had it on hold?
+			*currentBook.TimeUpdated = time.Now()
 		} else {
 			return nil, NoMatchError
 		}
@@ -333,14 +336,14 @@ func UpdateBook(c *gin.Context) {
 	currentState := book.State
 	incomingState := incomingRequest.State
 
-	// Input checking
-	if (incomingState != "available") && (incomingState != "on-hold") && (incomingState != "checked-out") {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Invalid state in incoming request."})
-		return
-	}
+	// // Input checking
+	// if (incomingState != "available") && (incomingState != "on-hold") && (incomingState != "checked-out") {
+	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Invalid state in incoming request."})
+	// 	return
+	// }
 
 	// Call the appropriate function from  the action table, and catch possible errors
-	book, err = actionTable[currentState][incomingState](book, incomingRequest)
+	book, err = actionTable[*currentState][*incomingState](book, incomingRequest)
 
 	if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
