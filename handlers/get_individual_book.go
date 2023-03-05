2c350675c5230b1df51db63cca216c4bf2d059a1
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-03-04 19:22:49 -0500

    Added underscores to filenames. Created separate files for get all books and get individual book functions.

--- handlers/get_individual_book.go
+++ handlers/get_individual_book.go
@@ -1,23 +1,9 @@
 package handlers
 
-import ( // h.Books, bookByISBN
-	"example/library_project/models"
-	
+import ( // h.Books, bookByISBN	
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
-
-// GET (all books)
-func (h *BooksHandler) GetAllBooks(c *gin.Context) {
-	// Make a slice containing all the values from mapOfBooks
-	var vals []*models.Book
-	
-	for _, v := range h.Books { // should change mapOfBooks to h.Books
-		vals = append(vals, v)
-	}
-
-	c.IndentedJSON(http.StatusOK, vals)
-}
 
 // GET (individual book)
 func (h *BooksHandler) GetIndividualBook(c *gin.Context) {
