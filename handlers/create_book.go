f74c78c7843069fe1a86b103fc93ec235bf46d52
Jordan Knorz <jordanknorz@Jordans-MBP-3.lan>
2023-03-05 22:42:01 -0500

    Added code block for decoding JSON to models.Book struct, and to call the pointer-receiver function Validate() defined on the models.Book struct. These code blocks were added to create_book.go and update_book.go, but were left commented out for later use.

--- handlers/create_book.go
+++ handlers/create_book.go
@@ -103,13 +103,33 @@ func (h *BooksHandler) CreateBook(c *gin.Context) {
 		TimeCreated: nil, 
 		TimeUpdated: nil,}
 
-	// Unmarshal
+	// Decoding JSON to map
 	incomingBookAsMap := map[string]interface{}{}
 	dec := json.NewDecoder(c.Request.Body)
 	if err := dec.Decode(&incomingBookAsMap); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
 		return
 	}
+
+///////////////////////////////////
+/////// FIRST MAJOR CHANGE ////////
+///////////////////////////////////
+
+	// // Decode JSON to book struct
+			// Notice this block is the same as the previous block with decoding to a map, except the first line
+			// Previously, the first line allocated memory for a map[string]interface{}{}, but here we allocae memory for a models.Book struct
+			// Also, in the 3rd line we pass book (rather than a pointer to the map)... note that the book variable is a pointer (see commment on that line)
+	// book := new(models.Book) // the "new" keyword allocates memory for models.Book, and returns a pointer to it
+	// dec := json.NewDecoder(c.Request.Body)
+	// if err := dec.Decode(book); err != nil {
+	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+	// 	return
+	// }
+
+//////////////////////////////////////////
+/////// End of FIRST MAJOR CHANGE ////////
+//////////////////////////////////////////
+
 
 	// Ensure that incoming JSON includes ISBN
 	if _, hasISBN := incomingBookAsMap["isbn"]; !hasISBN {
@@ -126,6 +146,22 @@ func (h *BooksHandler) CreateBook(c *gin.Context) {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
 		return
 	}
+
+///////////////////////////////////
+/////// SECOND MAJOR CHANGE ///////
+///////////////////////////////////
+
+	// // In the "FIRST MAJOR CHANGE", we decoded the JSON into a models.Book struct known as "book".
+		// // Recall that the book struct has a pointer-reciver function called Validate. So, we call that function and handle any errors that occur.
+	// if err := book.Validate(); err != nil {
+	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+	// 	return
+	// }
+
+//////////////////////////////////////////
+/////// End of SECOND MAJOR CHANGE ///////
+//////////////////////////////////////////
+	
 
 	// Validate semantics
 	if err := validateSemanticsForCreateBook(incomingBookAsMap); err != nil {
--- handlers/update_book.go
+++ handlers/update_book.go
@@ -239,6 +239,7 @@ func (h *BooksHandler) UpdateBook(c *gin.Context) {
 		return
 	}	
 
+	// Decoding JSON to map
 	incomingBookAsMap := map[string]interface{}{}
 	dec := json.NewDecoder(c.Request.Body)
 	if err := dec.Decode(&incomingBookAsMap); err != nil {
@@ -246,11 +247,47 @@ func (h *BooksHandler) UpdateBook(c *gin.Context) {
 		return
 	}
 
+///////////////////////////////////
+/////// FIRST MAJOR CHANGE ////////
+///////////////////////////////////
+
+	// // Decode JSON to book struct
+			// Notice this block is the same as the previous block with decoding to a map, except the first line
+			// Previously, the first line allocated memory for a map[string]interface{}{}, but here we allocae memory for a models.Book struct
+			// Also, in the 3rd line we pass book (rather than a pointer to the map)... note that the book variable is a pointer (see commment on that line)
+	// book := new(models.Book) // the "new" keyword allocates memory for models.Book, and returns a pointer to it
+	// dec := json.NewDecoder(c.Request.Body)
+	// if err := dec.Decode(book); err != nil {
+	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+	// 	return
+	// }
+
+//////////////////////////////////////////
+/////// End of FIRST MAJOR CHANGE ////////
+//////////////////////////////////////////
+
+
 	// Validate ISBN and State Syntax
 	if err := models.ValidateISBNAndStateSyntax(incomingBookAsMap); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
 		return
 	}
+
+///////////////////////////////////
+/////// SECOND MAJOR CHANGE ///////
+///////////////////////////////////
+
+	// // In the "FIRST MAJOR CHANGE", we decoded the JSON into a models.Book struct known as "book".
+		// // Recall that the book struct has a pointer-reciver function called Validate. So, we call that function and handle any errors that occur.
+	// if err := book.Validate(); err != nil {
+	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+	// 	return
+	// }
+
+//////////////////////////////////////////
+/////// End of SECOND MAJOR CHANGE ///////
+//////////////////////////////////////////
+
 
 	// Validate Time Semantics
 	if err := validateTimeSemanticsForUpdateBook(currentBook, incomingBookAsMap); err != nil {
