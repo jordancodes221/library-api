4e9e6274c5045cf45852e46fdcf22350d2609c76
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-03-26 13:14:38 -0400

    Moved the ValidateLogicForCreateBook function from book.go to create_book.go. Also had to change it to take *models.Book as an argument, rather than being a pointer-receiver on the Book struct.

--- models/book.go
+++ models/book.go
@@ -60,67 +60,6 @@ func (incomingBook *Book) Validate() (error) {
 	return nil
 }
 
-// ValidateLogicForCreateBook validates requests for the logic specific to creating a new book
-func (incomingBook *Book) ValidateLogicForCreateBook() (error) {
-	// Ensure ISBN is provided
-	if incomingBook.ISBN == nil {
-		return errors.New("Missing ISBN in the incoming request.")
-	}
-	
-	// Ensure state is provided
-	if incomingBook.State == nil {
-		return errors.New("Missing State in the incoming request.")
-	}
-
-	// CreateBook calls Validate(), which ensures *incomingBook.State (if provided) is equal to one "available", "on-hold", or "checked-out"
-	// Since we have already addressed the case where State is not provided, we know at this point State is equal to one of those 3 values.
-
-	// State is Available
-	if (*incomingBook.State == "available") {
-		if incomingBook.OnHoldCustomerID != nil {
-			return errors.New("Cannot have an on-hold customer ID when state is available.")
-		}
-
-		if incomingBook.CheckedOutCustomerID != nil {
-			return errors.New("Cannot have checked-out customer ID when state is available.")
-		}
-	}
-
-	// State is On-Hold
-	if (*incomingBook.State == "on-hold") {
-		if incomingBook.CheckedOutCustomerID != nil {
-			return errors.New("Cannot have checked-out customer ID when state is on-hold.")
-		}
-
-		if incomingBook.OnHoldCustomerID == nil {
-			return errors.New("State provided is on-hold, but no on-hold customer ID is provided.")
-		}
-	}
-
-	// State is Checked-Out
-	if (*incomingBook.State == "checked-out") {
-		if incomingBook.OnHoldCustomerID != nil {
-			return errors.New("Cannot have on-hold customer ID when state is checked-out.")
-		}
-
-		if incomingBook.CheckedOutCustomerID == nil {
-			return errors.New("State provided is checked-out, but no checked-out customer ID is provided.")
-		}
-	}
-
-	// Ensure TimeCreated is not provided by the client
-	if incomingBook.TimeCreated != nil {
-		return errors.New("Client cannot provide time created when creating a new book.")
-	}
-
-	// Ensure TimeUpdated is not provided by the client
-	if incomingBook.TimeUpdated != nil {
-		return errors.New("Client cannot provide time updated when creating a new book.")
-	}
-
-	return nil
-}
-
 // ValidateLogicForUpdateBook validates requests for the logic unique to updating an existing book
 func (incomingBook *Book) ValidateLogicForUpdateBook(currentBook *Book) (error) {	
 	// Ensure ISBN is provided
