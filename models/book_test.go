e76d1b6844ea151662f24c6b3945bdb396f1827e
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-04-10 08:10:11 -0400

    Imported the assert package from testify into book_test.go

--- models/book_test.go
+++ models/book_test.go
@@ -1,34 +1,35 @@
 package models
 
-// import (
-// 	"testing"
-// 	"time"
-// 	"example/library_project/utils"
-// )
+import (
+	"testing"
+	"time"
+	"example/library_project/utils"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestBook_Validate(t *testing.T){
-// 	tests := []struct{
-// 		description string
-// 		book *Book
-// 		expectedErrorMessage string
-// 	}{
-// 		{
-// 			description: "ISBN is the empty string", 
-// 			book: &Book{
-// 				ISBN: utils.ToPtr(""), 
-// 				State: utils.ToPtr("available"), 
-// 				OnHoldCustomerID: nil, 
-// 				CheckedOutCustomerID: nil, 
-// 				TimeCreated: utils.ToPtr(time.Now()), 
-// 				TimeUpdated: utils.ToPtr(time.Time{}),
-// 			}, 
-// 			expectedErrorMessage: "ISBN cannot be the empty string.",
-// 		},
-// 	}
+func TestBook_Validate(t *testing.T){
+	tests := []struct{
+		description string
+		book *Book
+		expectedErrorMessage string
+	}{
+		{
+			description: "ISBN is the empty string", 
+			book: &Book{
+				ISBN: utils.ToPtr(""), 
+				State: utils.ToPtr("available"), 
+				OnHoldCustomerID: nil, 
+				CheckedOutCustomerID: nil, 
+				TimeCreated: utils.ToPtr(time.Now()), 
+				TimeUpdated: utils.ToPtr(time.Time{}),
+			}, 
+			expectedErrorMessage: "ISBN cannot be the empty string.",
+		},
+	}
 
-// 	for _, currentTestCase := range tests {
-// 		t.Log(currentTestCase.description)
-// 		actual := currentTestCase.book.Validate()
-// 		assert.EqualError(t, actual, currentTestCase.expectedErrorMessage)
-// 	}
-// }
+	for _, currentTestCase := range tests {
+		t.Log(currentTestCase.description)
+		actual := currentTestCase.book.Validate()
+		assert.EqualError(t, actual, currentTestCase.expectedErrorMessage)
+	}
+}
