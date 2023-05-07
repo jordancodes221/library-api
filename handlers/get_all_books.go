773ac7c00c54f7eea96cc307d9dfe49344490d96
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-05-06 21:03:46 -0400

    GetAllBooks now calls ReadAll

--- handlers/get_all_books.go
+++ handlers/get_all_books.go
@@ -1,20 +1,18 @@
 package handlers
 
-import (
-	"example/library_project/models"
-	
+import (	
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 // GetAllBooks allows the client to get all of the books in the library
 func (h *BooksHandler) GetAllBooks(c *gin.Context) {
-	// Make a slice containing all the values from mapOfBooks
-	var vals []*models.Book
-	
-	for _, v := range h.Books { // should change mapOfBooks to h.Books
-		vals = append(vals, v)
+	all_books, err := h.ReadAll()
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"ERROR": err.Error()}) // 500 status code if unsuccessful
+		return
 	}
 
-	c.IndentedJSON(http.StatusOK, vals)
+	c.IndentedJSON(http.StatusOK, all_books)
 }
