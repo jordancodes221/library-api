9e3f514cca2ece8976e0dbaa22d86d4c72a83c15
Jordan Knorz <jordanknorz@Jordans-MBP-3.lan>
2023-05-09 19:00:50 -0400

    Added DateTimeInterface field to BooksHandler

--- handlers/books_handler.go
+++ handlers/books_handler.go
@@ -2,12 +2,14 @@ package handlers
 
 import (
 	"example/library_project/models"
+	"example/library_project/utils"
 )
 
 // BooksHandlers is the struct on which all handler functions are defined as pointer-receiver functions
 type BooksHandler struct {
 	// Books is the library of all the books
     Books map[string]*models.Book
+	DateTimeInterface utils.DateTimeProvider
 }
 
 func (h *BooksHandler) Create(newBook *models.Book) {
