970cd80c982cc503542dd251003a91dd0431fbbc
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-05-14 11:27:07 -0400

    Changed Books field of BooksHandler to type dao.BookDAO

--- handlers/books_handler.go
+++ handlers/books_handler.go
@@ -9,6 +9,6 @@ import (
 // BooksHandlers is the struct on which all handler functions are defined as pointer-receiver functions
 type BooksHandler struct {
 	// Books is the library of all the books
-    Books map[string]*models.Book
+	Books dao.BookDAO
 	DateTimeInterface utils.DateTimeProvider
 }
