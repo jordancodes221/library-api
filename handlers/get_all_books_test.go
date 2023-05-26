857dc7b7583d11673b222b2a538db1263e08a3cb
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-05-25 22:15:34 -0400

    Added the function TestBooksHandler_GetAllBooks

--- handlers/get_all_books_test.go
+++ handlers/get_all_books_test.go
@@ -1,1 +1,132 @@
 package handlers
+
+import (
+	"testing"
+	"encoding/json"
+	"time"
+	"example/library_project/utils"
+	"example/library_project/models"
+	"example/library_project/dao/inmemorydao"
+	"github.com/stretchr/testify/assert"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+
+	"fmt"
+)
+
+func TestBooksHandler_GetAllBooks(t *testing.T) {
+	arbitraryTime := time.Date(2023, 2, 1, 1, 30, 0, 0, time.UTC)
+
+	existingBook1 := &models.Book{
+		ISBN: utils.ToPtr("00001"), 
+		State: utils.ToPtr("available"), 
+		OnHoldCustomerID: nil, 
+		CheckedOutCustomerID: nil, 
+		TimeCreated: utils.ToPtr(arbitraryTime), 
+		TimeUpdated: nil,
+	}
+
+	existingBook2 := &models.Book{
+		ISBN: utils.ToPtr("00002"), 
+		State: utils.ToPtr("available"), 
+		OnHoldCustomerID: nil, 
+		CheckedOutCustomerID: nil, 
+		TimeCreated: utils.ToPtr(arbitraryTime), 
+		TimeUpdated: nil,
+	}
+
+	daoFactory := &inmemorydao.InMemoryDAOFactory{}
+	fixedTimeProvider := &utils.TestingDateTimeProvider{
+		ArbitraryTime: arbitraryTime,
+	}
+
+	h := NewBooksHandler(daoFactory, fixedTimeProvider)
+	h.BookDAOInterface.Create(existingBook1)
+	h.BookDAOInterface.Create(existingBook2)
+
+	tests := []struct{
+		description string
+		expectedStatusCode int
+		expectedBooks []*models.Book
+		expectedError *models.ErrorResponse
+	}{
+		{
+			description: "Successfully get all books",
+			expectedStatusCode: 200,
+			expectedBooks: []*models.Book{
+				&models.Book{
+					ISBN: utils.ToPtr("00001"),
+					State: utils.ToPtr("available"),
+					OnHoldCustomerID: nil,
+					CheckedOutCustomerID: nil,
+					TimeCreated: utils.ToPtr(arbitraryTime),
+					TimeUpdated: nil,
+				},
+				&models.Book{
+					ISBN: utils.ToPtr("00002"),
+					State: utils.ToPtr("available"),
+					OnHoldCustomerID: nil,
+					CheckedOutCustomerID: nil,
+					TimeCreated: utils.ToPtr(arbitraryTime),
+					TimeUpdated: nil,
+				},
+			},
+			expectedError: nil,
+		},
+	}
+
+	r := gin.Default()
+	r.GET("/books", h.GetAllBooks)
+
+	for _, currentTestCase := range tests {
+		fmt.Println(currentTestCase.description)
+		t.Log(currentTestCase.description)
+
+		req, err := http.NewRequest("GET", "/books", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, currentTestCase.expectedStatusCode, w.Code)
+
+		if currentTestCase.expectedBooks != nil {
+			// Decode response body into Book struct
+			actualBooks := new([]models.Book)
+			dec := json.NewDecoder(w.Body)
+			if err := dec.Decode(actualBooks); err != nil {
+				t.Fatal(err)
+			}
+
+			fmt.Println("")
+
+			fmt.Print("ACTUAL BOOKS: ")
+			fmt.Println(actualBooks)
+
+			fmt.Println("")
+
+			fmt.Print("EXPECTED BOOKS: ")
+			fmt.Println(currentTestCase.expectedBooks)
+
+			fmt.Println("")
+
+			assert.ElementsMatch(t, currentTestCase.expectedBooks, actualBooks)
+		}
+
+		if currentTestCase.expectedError != nil {
+			// Decode response body into ErrorResponse struct
+			actualError := new(models.ErrorResponse)
+			dec := json.NewDecoder(w.Body)
+			if err := dec.Decode(&actualError); err != nil {
+				t.Fatal(err)
+			}
+
+			// Check if actual error is equal to expected
+			assert.Equal(t, currentTestCase.expectedError, actualError)
+		}
+	}
+}
