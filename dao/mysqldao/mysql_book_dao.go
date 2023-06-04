107827cf97795a7a3d7c9e871982de8a49a87a6d
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-06-04 18:50:09 -0400

    Added the MySQLBookDAO struct and declared the functions required for it to implement the BookDAO interface

--- dao/mysqldao/mysql_book_dao.go
+++ dao/mysqldao/mysql_book_dao.go
@@ -1,1 +1,34 @@
 package mysqldao
+
+
+import (
+	"database/sql"
+	"example/library_project/models"
+
+	// "fmt"
+	// "log"
+)
+
+type MySQLBookDAO struct {
+	db *sql.DB
+}
+
+func (d *MySQLBookDAO) Create(newBook *models.Book) {
+	return
+}
+
+func (d *MySQLBookDAO) Delete(newBook *models.Book) {
+	return
+}
+
+func (d *MySQLBookDAO) Update(newBook *models.Book) {
+	return
+}
+
+func (d *MySQLBookDAO) Read(isbn string) (*models.Book, error) {
+	return nil, nil
+}
+
+func (d *MySQLBookDAO) ReadAll() ([]*models.Book, error) {
+	return nil, nil
+}
