95247d2e69390b7f69fb4734dd6ae650df54302d
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-06-05 21:53:05 -0400

    Added the ReadAll function body

--- dao/mysqldao/mysql_book_dao.go
+++ dao/mysqldao/mysql_book_dao.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"example/library_project/models"
 
-	// "fmt"
+	"fmt"
 	// "log"
 )
 
@@ -30,5 +30,31 @@ func (d *MySQLBookDAO) Read(isbn string) (*models.Book, error) {
 }
 
 func (d *MySQLBookDAO) ReadAll() ([]*models.Book, error) {
-	return nil, nil
+	query := "SELECT ISBN, State, OnHoldCustomerID, CheckedOutCustomerID, TimeCreated, TimeUpdated FROM Books"
+
+	rows, err := d.db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("error querying database: %v", err)
+	}
+	defer rows.Close()
+
+	retrievedBooks := make([]*models.Book, 0)
+
+	for rows.Next() {
+		nextBook := new(models.Book)
+
+		err := rows.Scan(
+			&nextBook.ISBN,
+			&nextBook.State,
+			&nextBook.OnHoldCustomerID,
+			&nextBook.CheckedOutCustomerID,
+			&nextBook.TimeCreated,
+			&nextBook.TimeUpdated,)
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
+		retrievedBooks = append(retrievedBooks, nextBook)
+	}
+
+	return retrievedBooks, nil
 }
