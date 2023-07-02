ca6c3db8ce41eb87f2f90e0164f42caa20eb61b5
Jordan Knorz <jordanknorz@Jordans-MacBook-Pro-3.local>
2023-07-02 15:28:24 -0400

    Added an error return type to the Create, Update, and Delete methods in the BookDAO interface

--- dao/book_dao.go
+++ dao/book_dao.go
@@ -6,9 +6,9 @@ import (
 
 type BookDAO interface {
 	// once a persistent database is added, these methods will also return an error type
-	Create(newBook *models.Book)
+	Create(newBook *models.Book) error
 	Read(isbn string) (*models.Book, error)
 	ReadAll() ([]*models.Book, error)
-	Update(book *models.Book)
-	Delete(book *models.Book)
+	Update(book *models.Book) error
+	Delete(book *models.Book) error
 }
