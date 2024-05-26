6a2bd7582334ad4a4435df165135aa7054c0f5f8
Jordan Knorz <jordanknorz@jordans-mbp-3.home>
2024-05-26 17:16:07 -0400

    Added InMemoryUserDAOFactory, constructor and defined the methods UserDAO, Open, Close and Clear

--- userdao/inmemoryuserdao/in_memory_user_dao_factory.go
+++ userdao/inmemoryuserdao/in_memory_user_dao_factory.go
@@ -1,2 +1,34 @@
 package inmemoryuserdao
 
+import (
+	"example/library_project/models"
+	"example/library_project/userdao"
+)
+
+type InMemoryUserDAOFactory struct {
+	Users map[string]*models.User
+}
+
+func NewInMemoryDAOFactory() *InMemoryUserDAOFactory {
+	return &InMemoryUserDAOFactory{
+		Users: map[string]*models.User{},
+	}
+}
+
+func (f *InMemoryUserDAOFactory) UserDAO() userdao.UserDAO {
+	return &InMemoryUserDAO{
+		Users: f.Users,
+	}
+}
+
+func (f *InMemoryUserDAOFactory) Open() error {
+	return nil
+}
+
+func (f *InMemoryUserDAOFactory) Close() error {
+	return nil
+}
+
+func (f *InMemoryUserDAOFactory) Clear() error {
+	return nil
+}
