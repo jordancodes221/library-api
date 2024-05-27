b487315b8b332f363d5aa3715eb710cfff74aed6
Jordan Knorz <jordanknorz@jordans-mbp-3.home>
2024-05-26 22:09:43 -0400

    first version of delete book handler function

--- userhandlers/delete_user.go
+++ userhandlers/delete_user.go
@@ -1,1 +1,31 @@
 package userhandlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// DeleteUser allows the client to delete a user from the library
+func (h *UsersHandler) DeleteUser(c *gin.Context) {
+	username := c.Param("username")
+
+	user, err := h.UserDAOInterface.Read(username)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"ERROR": err.Error()}) // 500 status code
+		return
+	}
+
+	if user == nil {
+		c.Status(http.StatusNoContent)
+		return
+	}
+
+	if err := h.UserDAOInterface.Delete(user); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"ERROR": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
